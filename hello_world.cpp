52eb2b6f8bf6368ca6e9a6b002653d730dd341ae
sekhmych <sekhmych@gmail.com>
2024-09-30 02:07:58 +0000

    changed hello_world.cpp Now its get user_name and write - Hello world from @user_name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
+	string name;
+	cout << "Please, enter your name: ";
+	cin >> name;
+	cout << "Hello world from " << name << endl;
 
 	return 0;
 }
