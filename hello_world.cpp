067250569be97206089974c5a174bae5de3affc2
sekhmych <sekhmych@gmail.com>
2024-09-30 02:11:00 +0000

    remove using namespace std for for good code reasons

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main() {
-	string name;
-	cout << "Please, enter your name: ";
-	cin >> name;
-	cout << "Hello world from " << name << endl;
+	std::string name;
+	std::cout << "Please, enter your name: ";
+	std::cin >> name;
+	std::cout << "Hello world from " << name << std::endl;
 
 	return 0;
 }
